56b4003c188ef9789fa6ec3ce12fc1baf7802093
NicheApp <nishkarshmaitry0864@gmail.com>
2021-01-15 01:00:11 +0530

    new change

--- app/src/main/java/com/mj/fooddeliveryapp/fragments/FoodAccount.java
+++ app/src/main/java/com/mj/fooddeliveryapp/fragments/FoodAccount.java
@@ -1,7 +1,26 @@
 package com.mj.fooddeliveryapp.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.mj.fooddeliveryapp.R;
+import com.mj.fooddeliveryapp.writedata;
 
 //Hellp therre again merge again
 public class FoodAccount  extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.account, container, false);
+
+
+        return  view;
+    }
+
+
 }
